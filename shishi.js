4e5de52b1110d5bb0fb9ba935bda47d4985538a4
tb536336 <62318335+tb536336@users.noreply.github.com>
2021-10-23 23:15:54 +0800

    Update shishi.js

--- shishi.js
+++ shishi.js
@@ -1,8 +1,9 @@
-const vip = "/user/getUser/";
+
 let obj = JSON.parse($response.body);
 
-if ($request.url.indexOf(vip) != -1){
-obj.data.uinfo["vipType:"] = "2";
-obj.data.uinfo["vipTime:"] = "1111";
+obj = {
+  "vipType": 3,
+    "vipTime": 99999,
+ };
 
 $done({body: JSON.stringify(obj)});
